3e6eeffb94fd12c29b29ee1bb42b938c7dde0156
Sultan <bekovs96@gmail.com>
2022-08-12 00:48:29 +0600

    footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ProductContextProvider from "./contexts/ProductContextProvider";
 import MainRoutes from "./MainRoutes";
 import { Box } from "@mui/material";
 import CartContextProvider from "./contexts/CartContextProvider";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
               </Box>
               <Box sx={{width: '80%'}}>
                 <MainRoutes />
+              </Box>
+              <Box sx={{position: 'absolute', bottom: '0', width: '100%', zIndex: '2000'}}>
+                <Footer />
               </Box>
             </Box>
           </CartContextProvider>
