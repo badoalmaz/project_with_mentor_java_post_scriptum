0d760e0717ee17f5134beb5c4778eb13a620efdf
Bado Almaz <badoalmaz@Bados-MacBook-Pro.local>
2022-08-11 12:08:28 +0600

    cart 3

--- src/contexts/CartContextProvider.js
+++ src/contexts/CartContextProvider.js
@@ -1,8 +1,13 @@
 import React, { createContext, useContext, useReducer } from "react";
 import { CART } from "../helpers/consts";
-import { getCountProductsInCart } from "../helpers/functions";
+import {
+  calcSubPrice,
+  calcTotalPrice,
+  getCountProductsInCart,
+} from "../helpers/functions";
 
 const cartContext = createContext();
+
 export const useCart = () => {
   return useContext(cartContext);
 };
@@ -46,9 +51,100 @@ const CartContextProvider = ({ children }) => {
     });
   };
 
-  const addProductToCart = (product) => {};
+  const addProductToCart = (product) => {
+    let cart = JSON.parse(localStorage.getItem("cart"));
 
-  return <cartContext.Provider>{children}</cartContext.Provider>;
+    if (!cart) {
+      cart = {
+        products: [],
+        totalPrice: 0,
+      };
+    }
+
+    let newProduct = {
+      item: product,
+      count: 1,
+      subPrice: +product.price,
+    };
+
+    let productToFind = cart.products.filter(
+      (elem) => elem.item.id === product.id
+    );
+    // если не найдется элемент в localStorage, то он добавит элемент в localStorage,а если найдет, то удалит
+    if (productToFind.length === 0) {
+      cart.products.push(newProduct);
+    } else {
+      cart.products = cart.products.filter(
+        (elem) => elem.item.id !== product.id
+      );
+    }
+
+    cart.totalPrice = calcTotalPrice(cart.products);
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    dispatch({
+      type: CART.GET_CART,
+      payload: cart,
+    });
+  };
+
+  function deleteProductInCart(id) {
+    let cart = JSON.parse(localStorage.getItem("cart"));
+
+    cart.products = cart.products.filter((elem) => elem.item.id !== id);
+    cart.totalPrice = calcTotalPrice(cart.products);
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    getCart();
+    dispatch({
+      type: CART.GET_CART_LENGTH,
+      payload: cart,
+    });
+  }
+
+  const changeProductCount = (count, id) => {
+    let cart = JSON.parse(localStorage.getItem("cart"));
+
+    cart.products = cart.products.map((product) => {
+      if (product.item.id === id) {
+        product.count = count;
+        product.subPrice = calcSubPrice(product);
+      }
+      return product;
+    });
+    cart.totalPrice = calcTotalPrice(cart.products);
+
+    dispatch({
+      type: CART.GET_CART,
+      payload: cart,
+    });
+  };
+
+  function checkProductInCart(id) {
+    let cart = JSON.parse(localStorage.getItem("cart"));
+
+    if (cart) {
+      let newCart = cart.products.filter((elem) => elem.item.id === id);
+      return newCart.length > 0 ? true : false;
+    } else {
+      cart = {
+        product: [],
+        totalPrice: 0,
+      };
+    }
+  }
+
+  const values = {
+    getCart,
+    addProductToCart,
+    changeProductCount,
+    checkProductInCart,
+    deleteProductInCart,
+    cart: state.cart,
+  };
+
+  return <cartContext.Provider value={values}>{children}</cartContext.Provider>;
 };
 
 export default CartContextProvider;
